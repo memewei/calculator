ed0a92df96db966ff475e4bc7549e3832818d02b
memewei <minwei2002@gmail.com>
2023-09-13 16:43:09 +0800

    Write calculator logic

---
+++ script.js
@@ -0,0 +1,128 @@
+let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
+let shouldResetScreen = false;
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.getElementById("equal");
+const clearButton = document.getElementById("clear");
+const decimalButton = document.querySelector("#decimal");
+const previousOperationScreen = document.querySelector(".prev-screen");
+const currentOperationScreen = document.querySelector(".current-screen");
+
+window.addEventListener("keydown", handleKeyboardInput);
+equalsButton.addEventListener("click", evaluate);
+clearButton.addEventListener("click", clear);
+decimalButton.addEventListener("click", appendDecimal);
+
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => appendNumber(button.textContent))
+);
+
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", () => setOperation(button.textContent))
+);
+
+function appendNumber(number){
+  if(currentOperationScreen.textContent === "0" || shouldResetScreen){
+    resetScreen();
+  }
+  currentOperationScreen.textContent += number;
+}
+
+function resetScreen(){
+  currentOperationScreen.textContent = "";
+  shouldResetScreen = false;
+}
+
+function clear(){
+  currentOperationScreen.textContent = "0";
+  previousOperationScreen.textContent = "";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
+}
+
+function appendDecimal(){
+  if(shouldResetScreen) resetScreen();
+  if(currentOperationScreen.textContent === ""){
+    currentOperationScreen.textContent = "0";
+  }
+  if(currentOperationScreen.textContent.includes(".")) return;
+  currentOperationScreen.textContent += ".";
+}
+
+function setOperation(operator){
+  if(currentOperation !== null) evaluate();
+  firstOperand = currentOperationScreen.textContent;
+  currentOperation = operator;
+  previousOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+  shouldResetScreen = true;
+}
+
+function evaluate(){
+  if(currentOperation === null || shouldResetScreen) return;
+  if(currentOperation === "÷" && currentOperationScreen.textContent === "0"){
+    alert("You can't divide by 0!");
+    return;
+  }
+  secondOperand = currentOperationScreen.textContent;
+  currentOperationScreen.textContent = roundResult(operate(currentOperation, firstOperand, secondOperand));
+  previousOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+  currentOperation = null;
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000;
+}
+
+function handleKeyboardInput(e){
+  if(e.key >= 0 && e.key <= 9) appendNumber(e.key);
+  if(e.key === ".") appenddecimal();
+  if(e.key === "=" || e.key === "Enter") evaluate();
+  if(e.key === "Backspace") deleteNumber();
+  if(e.key === "Escape") clear();
+  if(e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") setOperation(convertOperator(e.key));
+}
+
+function convertOperator(keyboardOperator){
+  if(keyboardOperator === "/") return "÷";
+  if(keyboardOperator === "*") return "×";
+  if(keyboardOperator === "-") return "−";
+  if(keyboardOperator === "+") return "+";
+}
+
+function add(a, b){
+  return a + b;
+}
+
+function substract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+function operate(operator, a, b){
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "−":
+      return substract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
+}
